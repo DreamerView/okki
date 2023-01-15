0a46f16287d0213d5c60b4efbdb26cc3240e5c04
Dreamer <temirkhan.onyx@gmail.com>
2023-01-15 23:53:21 +0600

    adssadads

--- pages/user/headerModule.js
+++ pages/user/headerModule.js
@@ -36,7 +36,7 @@ const HeaderUser = ({lang}) => {
                         </div>
                     </div>
                     <div className={`${style.main__block_user_row}`}>
-                        <Link href="/user/history">
+                        <Link href="/user/history" prefetch={false}>
                         <div className={`${style.main__block_user} anim_hover`}>
                             <div className={`${style.main__block_user_image} blue_background`}>
                                 <div className={style.main__block_user_image_row}>
@@ -46,7 +46,7 @@ const HeaderUser = ({lang}) => {
                             <p>{translate['recent'][lang]}</p>
                         </div>
                         </Link>
-                        <Link href="/user/favourite">
+                        <Link href="/user/favourite" prefetch={false}>
                         <div className={`${style.main__block_user} anim_hover`}>
                             <div className={`${style.main__block_user_image} red_background`}>
                                 <div className={style.main__block_user_image_row}>
@@ -56,7 +56,7 @@ const HeaderUser = ({lang}) => {
                             <p>{translate['favorites'][lang]}</p>
                         </div>
                         </Link>
-                        <Link href="/user/device">
+                        <Link href="/user/device" prefetch={false}>
                         <div className={`${style.main__block_user} anim_hover`}>
                             <div className={`${style.main__block_user_image} green_background`}>
                                 <div className={style.main__block_user_image_row}>
