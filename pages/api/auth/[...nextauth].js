a61966cdac7ae6a4bc6316a020e9357f8adce315
Dreamer <temirkhan.onyx@gmail.com>
2023-01-23 15:25:07 +0600

    dsasdadsadas

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,11 +6,13 @@ export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      state: false
     }),
     VkProvider({
       clientId: process.env.VK_CLIENT_ID,
-      clientSecret: process.env.VK_CLIENT_SECRET
+      clientSecret: process.env.VK_CLIENT_SECRET,
+      state: false
     })
   ],
   callbacks: {
