83c605b3b4c0e836eed9b3c16e811fb31380ae0e
Dreamer <temirkhan.onyx@gmail.com>
2023-02-18 23:35:01 +0600

    adssdasad

--- pages/index_content.js
+++ pages/index_content.js
@@ -21,18 +21,20 @@ const IndexContent = ({lang,service,styles,translate,nav_translate,Link,Image}) 
   };
   const toRightScroll = () => {
     document.querySelector(".box-inner").scrollBy({left:324,behavior: 'smooth'});
-    setScrollResult(document.querySelector(".box-inner").scrollLeft);
-  }
+    console.log(document.querySelector(".box-inner").scrollLeft);
+    setScrollResult(prev=>prev=document.querySelector(".box-inner").scrollLeft);
+  };
   const toLeftScroll = () => {
     document.querySelector(".box-inner").scrollBy({left:-324,behavior: 'smooth'});
-    setScrollResult(document.querySelector(".box-inner").scrollLeft);
-  }
+    console.log(document.querySelector(".box-inner").scrollLeft);
+    setScrollResult(prev=>prev=document.querySelector(".box-inner").scrollLeft);
+  };
   return(
         <>
         <div className="main block_animation">
           <div className="main_block_row">
             <h1 className="flex_text">{translate['popular'][locale]} <div className="emoji_h1"><Image title={`Microsoft fire emoji (Used for informational purposes only)`} priority src={"/emoji-small/fire.webp"} width={26} height={26} alt="emoji"/></div></h1>
-            <p className="sub_content">{translate['popular_subtext'][locale]}</p>
+            <p className="sub_content">{translate['popular_subtext'][locale]} {scrollResult}</p>
             <div className={styles.main__index_b}>
               {lazy===true && scrollResult!==null && scrollResult<=300?"":
               <div className={`${styles.left} arrow-left arrow anim_hover`} onClick={toLeftScroll}><Image width={32} height={32} src="/img/arrow_left.svg" alt="arrow-left"/></div>}
