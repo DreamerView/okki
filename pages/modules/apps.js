d7fae2057ec344372ed33e1e87aef3cde1f38dcc
Dreamer <temirkhan.onyx@gmail.com>
2023-02-19 21:57:32 +0600

    asdasdasda

--- pages/modules/apps.js
+++ pages/modules/apps.js
@@ -20,7 +20,7 @@ const AppStore = ({serv,Link,nav_translate,lang,Image}) =>{
         content.current!==undefined&&content.current!==null&&content.current.removeEventListener('scroll', onScroll);
         };
     },[]);
-    const group = (items, n) => items.filter(e=>e.type === 'services').reduce((acc, x, i) => {
+    const group = (items, n) => items.filter(e=>e.type === 'services').reverse().reduce((acc, x, i) => {
         const idx = Math.floor(i / n);
         acc[idx] = [...(acc[idx] || []), x];
         return acc;
