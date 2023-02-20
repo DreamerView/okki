54612d52a47c561df2fe9c157ba2ab0cba8c4fd9
Dreamer <temirkhan.onyx@gmail.com>
2023-02-21 00:18:05 +0600

    adsasdasddas

--- pages/modules/app.js
+++ pages/modules/app.js
@@ -30,7 +30,7 @@ const AppShow = ({name,translate,Image,lang}) => {
             </div>
             <div className="main__app_info_banner_block">
                 <p className="small">Release</p>
-                <h1>☁</h1>
+                <h1>⊙</h1>
                 <p className="small">12.08.2022</p>
             </div>
         </div>
