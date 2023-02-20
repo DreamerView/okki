d28c444ed62b7a31d9fdc05d0b991778ed9fffd0
Dreamer <temirkhan.onyx@gmail.com>
2023-02-20 21:13:43 +0600

    adsdasdasadsads

--- pages/modules/app.js
+++ pages/modules/app.js
@@ -4,10 +4,10 @@ const AppShow = ({name,translate,Image,lang}) => {
         <div className="main__app_info">
             <Image width={60} height={60} alt="icon" src={"/services/"+name+".webp"}/>
             <div className="main__app_info_block">
-                <h5>{translate}</h5>
+                <h2>{translate}</h2>
                 <p className="smaller">Здоровье</p>
+                <button className="main__app_info_action anim_hover">{ux['save'][lang]}</button>
             </div>
-            <button className="main__app_info_action anim_hover">{ux['save'][lang]}</button>
         </div>
     );
 };
