3eaa4e10ea38f3e2e1d934e49e9dbcf7783506bd
Dreamer <temirkhan.onyx@gmail.com>
2023-02-20 23:22:04 +0600

    adsasdasd

--- pages/modules/app.js
+++ pages/modules/app.js
@@ -1,15 +1,34 @@
 import ux from "/translate/ux/action"
 const AppShow = ({name,translate,Image,lang}) => {
     return(
+        <div className="main__app_i">
         <div className="main__app_info">
             <Image width={60} height={60} alt="icon" src={"/services/"+name+".webp"}/>
             <div className="main__app_info_block">
                 <div>
-                <h1>{translate}</h1>
+                <h3>{translate}</h3>
                 <p className="smaller">Рейтинг 5.0 ★</p>
                 </div>
                 <button className="main__app_info_action anim_hover">{ux['save'][lang]}</button>
             </div>
+        </div>
+        <div className="main__app_info_banner">
+            <div className="main__app_info_banner_block">
+                <p className="small">Rating</p>
+                <h1>4.8</h1>
+                <p className="small">★★★★★</p>
+            </div>
+            <div className="main__app_info_banner_block">
+                <p className="small">Language</p>
+                <h1>EN</h1>
+                <p className="small">+ 2 More</p>
+            </div>
+            <div className="main__app_info_banner_block">
+                <p className="small">Rating</p>
+                <h1>4.8</h1>
+                <p className="small">★★★★★</p>
+            </div>
+        </div>
         </div>
     );
 };
