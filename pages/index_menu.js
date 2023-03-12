d4c129408a8132767b082a150851f52a97b7ae08
Dreamer <temirkhan.onyx@gmail.com>
2023-03-12 15:15:58 +0600

    adsdsadasdas

--- pages/index_menu.js
+++ pages/index_menu.js
@@ -4,18 +4,18 @@ const IndexMenu = ({lang,service,styles,translate,nav_translate,Link,Image}) => 
     const locale =lang,serv = service!==undefined?service:[{}];
     return(
         <>
-            <div itemScope itemType="https://schema.org/BreadcrumbList" className={`${styles.main__menu_nav} block_animation`}>
-            <div className={styles.main__menu_nav_blocks}>
+            <div itemScope itemType="https://schema.org/BreadcrumbList" className={`${styles!==undefined&&styles.main__menu_nav} block_animation`}>
+            <div className={styles!==undefined&&styles.main__menu_nav_blocks}>
             {(serv)&&serv.filter(e=>e.type === 'category').map((e,index)=>
                 <div itemProp="itemListElement" itemScope itemType="https://schema.org/ListItem" key={index+1}>
-                <Link title={nav_translate[e.name][locale]} itemID={e.location} itemType="https://schema.org/Thing"  itemScope itemProp="item" href={e.location} prefetch={false}>
-                    <div className={`${styles.main__menu_nav_block} anim_hover`}>
-                    <div className={`${styles.main__menu_nav_block_image} ${e.image_background}`}>
-                        <div className={`${styles.main__menu_nav_block_image_pic}`}>
-                        <Image priority title={translate[e.name][locale]} width={36} height={36} src={e.image} alt="icon"/>
+                <Link title={nav_translate!==undefined&&nav_translate[e.name][locale]} itemID={e.location} itemType="https://schema.org/Thing"  itemScope itemProp="item" href={e.location} prefetch={false}>
+                    <div className={`${styles!==undefined&&styles.main__menu_nav_block} anim_hover`}>
+                    <div className={`${styles!==undefined&&styles.main__menu_nav_block_image} ${e.image_background}`}>
+                        <div className={`${styles!==undefined&&styles.main__menu_nav_block_image_pic}`}>
+                        <Image priority title={translate!==undefined&&translate[e.name][locale]} width={36} height={36} src={e.image} alt="icon"/>
                         </div>
                     </div>
-                    <span className={styles.nav_header} itemProp="name">{nav_translate[e.name][locale]}</span>
+                    <span className={styles!==undefined&&styles.nav_header} itemProp="name">{nav_translate!==undefined&&nav_translate[e.name][locale]}</span>
                     </div>
                 </Link>
                 <meta itemProp="position" content={index+1} />
