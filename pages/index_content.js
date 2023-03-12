1a544c769ac10bc44ac46ecccf685c04765ce461
Dreamer <temirkhan.onyx@gmail.com>
2023-03-12 14:43:05 +0600

    dsadasadsaddasdas

--- pages/index_content.js
+++ pages/index_content.js
@@ -34,28 +34,28 @@ const IndexContent = ({locale,service,styles,translate,nav_translate,Link,Image,
           <div className="main_block_row">
             <h1 className="flex_text">{translate!==undefined&&translate['popular'][locale]} <div className="emoji_h1"><Image title={`Microsoft fire emoji (Used for informational purposes only)`} priority src={"/emoji-small/fire.webp"} width={26} height={26} alt="emoji"/></div></h1>
             <p className="sub_content">{translate!==undefined&&translate['popular_subtext'][locale]}</p>
-            <div className={styles!==undefined&&styles.main__index_b}>
+            <div className={styles!==undefined&&styles!==undefined&&styles!==undefined&&styles.main__index_b}>
               {lazy===true && offset<=10?"":
-              <div className={`${styles.left} arrow-left arrow anim_hover`} onClick={toLeftScroll}><Image width={32} height={32} src="/img/arrow_left.svg" alt="arrow-left"/></div>}
-              <div className={`${styles.right} arrow-right arrow anim_hover`} onClick={toRightScroll}><Image width={32} height={32} src="/img/arrow_right.svg" alt="arrow-right"/></div>
-              <div className={`${styles.main__index_block_row} box-inner`} ref={banner}>
+              <div className={`${styles!==undefined&&styles.left} arrow-left arrow anim_hover`} onClick={toLeftScroll}><Image width={32} height={32} src="/img/arrow_left.svg" alt="arrow-left"/></div>}
+              <div className={`${styles!==undefined&&styles.right} arrow-right arrow anim_hover`} onClick={toRightScroll}><Image width={32} height={32} src="/img/arrow_right.svg" alt="arrow-right"/></div>
+              <div className={`${styles!==undefined&&styles.main__index_block_row} box-inner`} ref={banner}>
                 {serv&&serv.filter(e=>e.type === 'services').reverse().map((e,index)=>
-                <Link onClick={()=>historyAction(e.name)} title={nav_translate[e.name][locale]} href={e.location} prefetch={false} key={index+1}>
-                  <div className={`${styles.main__index_block_row_b}`}>
-                    <div className={styles.main__index_block}>
-                      <div className={styles.main__index_block_pic_info}>
-                        <Image priority title={nav_translate[e.name][locale]} alt={`Illustration by <a href="https://icons8.com/illustrations/author/zD2oqC8lLBBA">Icons 8</a> from <a href="https://icons8.com/illustrations">Ouch!</a>`} width={60} className={styles.main__index_block_pic_info_img} height={60} src={e.image}/>
-                        <div className={styles.main__index_block_pic_info_block}>
-                          <h6>{nav_translate[e.name][locale]}</h6>
-                          <span className="head">{nav_translate[e.category][locale]}</span>
+                <Link onClick={()=>historyAction(e.name)} title={nav_translate!==undefined&&nav_translate[e.name][locale]} href={e.location} prefetch={false} key={index+1}>
+                  <div className={`${styles!==undefined&&styles.main__index_block_row_b}`}>
+                    <div className={styles!==undefined&&styles.main__index_block}>
+                      <div className={styles!==undefined&&styles.main__index_block_pic_info}>
+                        <Image priority title={nav_translate!==undefined&&nav_translate[e.name][locale]} alt={`Illustration by <a href="https://icons8.com/illustrations/author/zD2oqC8lLBBA">Icons 8</a> from <a href="https://icons8.com/illustrations">Ouch!</a>`} width={60} className={styles!==undefined&&styles.main__index_block_pic_info_img} height={60} src={e.image}/>
+                        <div className={styles!==undefined&&styles.main__index_block_pic_info_block}>
+                          <h6>{nav_translate!==undefined&&nav_translate[e.name][locale]}</h6>
+                          <span className="head">{nav_translate!==undefined&&nav_translate[e.category][locale]}</span>
                         </div>
-                        <div className={styles.main__index_block_pic_info_block_action}>
+                        <div className={styles!==undefined&&styles.main__index_block_pic_info_block_action}>
                           Open
                         </div>
                       </div>
-                      <div className={styles.main__index_block_pic_back}></div>
-                      <div className={styles.main__index_block_pic}>
-                        <Image priority title={nav_translate[e.name][locale]} alt={`Illustration by <a href="https://icons8.com/illustrations/author/zD2oqC8lLBBA">Icons 8</a> from <a href="https://icons8.com/illustrations">Ouch!</a>`} width={160} height={160} className={styles.main__index_block_img} src={e.image}/>
+                      <div className={styles!==undefined&&styles.main__index_block_pic_back}></div>
+                      <div className={styles!==undefined&&styles.main__index_block_pic}>
+                        <Image priority title={nav_translate!==undefined&&nav_translate[e.name][locale]} alt={`Illustration by <a href="https://icons8.com/illustrations/author/zD2oqC8lLBBA">Icons 8</a> from <a href="https://icons8.com/illustrations">Ouch!</a>`} width={160} height={160} className={styles!==undefined&&styles.main__index_block_img} src={e.image}/>
                       </div>
                     </div>
                   </div>
