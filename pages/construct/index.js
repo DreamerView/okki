9a6254810b8116b7df7e0dd946a96a6153e1646e
Dreamer <temirkhan.onyx@gmail.com>
2023-03-14 20:51:51 +0600

    dsadsasdads

--- pages/construct/index.js
+++ pages/construct/index.js
@@ -1,16 +1,16 @@
 /*jshint esversion: 6 */
 import Image from "next/image";
 import Head from "next/head";
-import Link from "next/link";
+// import Link from "next/link";
 import translate from "/translate/constructor/acc/navbar_translate";
 import style from "/styles/constructor/index.module.css";
 import nav_translate from "/translate/services/all_translate";
 import seo from "/translate/constructor/index_seo";
 import AllService from '/start/services/all.json';
 import dynamic from "next/dynamic";
-const AppStorePreloader = dynamic(()=>import("/modules/app_store/apps_preloader"),{ssr:false});
+import AppStorePreloader from "/modules/app_store/apps_preloader";
 const AppStore =  dynamic(()=>import("/modules/app_store/apps"),{loading: AppStorePreloader});
-const NavPreloader = dynamic(()=>import("/modules/navbar_app/nav_preloader"),{ssr:false});
+import NavPreloader from "/modules/navbar_app/nav_preloader";
 const NavbarApp = dynamic(()=>import('/modules/navbar_app/nav'),{ssr:false,loading:NavPreloader});
 const ConstructorIndex = ({lang}) => {
     return(
@@ -32,11 +32,11 @@ const ConstructorIndex = ({lang}) => {
                 <meta name="twitter:image" content={process.env.hostName+"/seo_image/twitter.webp"}/>
                 <link rel="image_src" href={process.env.hostName+"/seo_image/twitter.webp"}/>
             </Head>
-            <NavbarApp to={{href:"/"}} choice="alone" lang={lang}/>
+            {/* <NavbarApp to={{href:"/"}} choice="alone" lang={lang}/> */}
             <div className="main_app block_animation">
             <h1 className="flex_text">{translate["step0"][lang]} <div className="emoji_h1"><Image title={'Microsoft fire emoji (Used for informational purposes only)'} priority src={"/emoji-small/fire.webp"} width={26} height={26} alt="emoji"/></div></h1>
             <p className="sub_content">{translate["step0_description"][lang]}</p>
-            <AppStore category="constructor" serv={AllService} style={style} Link={Link} nav_translate={nav_translate} lang={lang} Image={Image}/>
+            {/* <AppStore category="constructor" serv={AllService} style={style} Link={Link} nav_translate={nav_translate} lang={lang} Image={Image}/> */}
         </div>
       </>
     );
