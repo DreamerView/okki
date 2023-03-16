dbc244f94a923e7f2dc392de03d175130426e669
Dreamer <temirkhan.onyx@gmail.com>
2023-03-16 10:38:00 +0600

    asddsaasdsda

--- pages/_app.js
+++ pages/_app.js
@@ -7,12 +7,15 @@ import "/styles/preloader.css";
 const Preloader = dynamic(()=>import("/modules/preloader"),{ssr:false});
 
 const AppModule = dynamic(()=>import('/modules/app_module'));
+const DocumentResult = dynamic(()=>import("/start/document"),{loading:Preloader});
 
 const MyApp = ({ Component, pageProps, session }) => {
     const [result,setResult] = useState(prev=>prev=false);
     return(
             <AppModule change={(res)=>setResult(prev=>prev=res)} session={session}>
+                <DocumentResult>
                     {result? <Preloader/>:<Component {...pageProps} />}
+                </DocumentResult>
             </AppModule>
     )
 }
