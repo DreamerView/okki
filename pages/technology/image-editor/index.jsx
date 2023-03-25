f59067a093e15c113c3fc927a4266e100c846c0d
Dreamer <temirkhan.onyx@gmail.com>
2023-03-26 02:00:23 +0600

    adsdsaasdsad

--- pages/technology/image-editor/index.jsx
+++ pages/technology/image-editor/index.jsx
@@ -29,7 +29,7 @@ const ImageEditor = ({lang}) => {
     },[range]);
     useEffect(()=>{
         imageEditor.current.style.transform = `rotate(${rotate}deg)`;
-    },[rotate])
+    },[rotate]);
     return(
         <>
         <NavbarApp lang={lang} choice="alone"/>
@@ -51,49 +51,49 @@ const ImageEditor = ({lang}) => {
                 </div>
                 {nav==="correction"&&<><div className={`${style.editor_block} block_animation`}>
                     <div onClick={()=>setSelectChoice(prev=>prev='blur')} className={`${style.editor_block_button} `}>
-                        <div className={selected==="blur"?style.editor_block_button_icon_active:params.blur!==range.blur?style.editor_block_button_icon_changed:style.editor_block_button_icon}>
+                        <div className={selected==="blur"?style.editor_block_button_icon_active:Number(params.blur)===Number(range.blur)?style.editor_block_button_icon:style.editor_block_button_icon_changed}>
                             <Image src="/img/blur.svg" width="36" height="36" alt="icon" />
                         </div>
                         <h6>Blur</h6>
                     </div>
                     <div onClick={()=>setSelectChoice(prev=>prev='brightness')} className={`${style.editor_block_button} `}>
-                        <div className={selected==="brightness"?style.editor_block_button_icon_active:params.brightness!==range.brightness?style.editor_block_button_icon_changed:style.editor_block_button_icon}>
+                        <div className={selected==="brightness"?style.editor_block_button_icon_active:Number(params.brightness)!==Number(range.brightness)?style.editor_block_button_icon_changed:style.editor_block_button_icon}>
                             <Image src="/img/brightness.svg" width="36" height="36" alt="icon" />
                         </div>
                         <h6>Brightness</h6>
                     </div>
                     <div onClick={()=>setSelectChoice(prev=>prev='contrast')} className={`${style.editor_block_button} `}>
-                        <div className={selected==="contrast"?style.editor_block_button_icon_active:params.contrast!==range.contrast?style.editor_block_button_icon_changed:style.editor_block_button_icon}>
+                        <div className={selected==="contrast"?style.editor_block_button_icon_active:Number(params.contrast)!==Number(range.contrast)?style.editor_block_button_icon_changed:style.editor_block_button_icon}>
                             <Image src="/img/contrast.svg" width="36" height="36" alt="icon" />
                         </div>
                         <h6>Contrast</h6>
                     </div>
                     <div onClick={()=>setSelectChoice(prev=>prev='grayscale')} className={`${style.editor_block_button} `}>
-                        <div className={selected==="grayscale"?style.editor_block_button_icon_active:params.grayscale!==range.grayscale?style.editor_block_button_icon_changed:style.editor_block_button_icon}>
+                        <div className={selected==="grayscale"?style.editor_block_button_icon_active:Number(params.grayscale)!==Number(range.grayscale)?style.editor_block_button_icon_changed:style.editor_block_button_icon}>
                             <Image src="/img/contrast.svg" width="36" height="36" alt="icon" />
                         </div>
                         <h6>Grayscale</h6>
                     </div>
                     <div onClick={()=>setSelectChoice(prev=>prev='hue')} className={`${style.editor_block_button} `}>
-                        <div className={selected==="hue"?style.editor_block_button_icon_active:params.hue!==range.hue?style.editor_block_button_icon_changed:style.editor_block_button_icon}>
+                        <div className={selected==="hue"?style.editor_block_button_icon_active:Number(params.hue)!==Number(range.hue)?style.editor_block_button_icon_changed:style.editor_block_button_icon}>
                             <Image src="/img/hue.svg" width="36" height="36" alt="icon" />
                         </div>
                         <h6>Hue-rotate</h6>
                     </div>
                     <div onClick={()=>setSelectChoice(prev=>prev='invert')} className={`${style.editor_block_button} `}>
-                        <div className={selected==="invert"?style.editor_block_button_icon_active:params.invert!==range.invert?style.editor_block_button_icon_changed:style.editor_block_button_icon}>
+                        <div className={selected==="invert"?style.editor_block_button_icon_active:Number(params.invert)!==Number(range.invert)?style.editor_block_button_icon_changed:style.editor_block_button_icon}>
                             <Image src="/img/hue.svg" width="36" height="36" alt="icon" />
                         </div>
                         <h6>Invert</h6>
                     </div>
                     <div onClick={()=>setSelectChoice(prev=>prev='saturate')} className={`${style.editor_block_button} `}>
-                        <div className={selected==="saturate"?style.editor_block_button_icon_active:params.saturate!==range.saturate?style.editor_block_button_icon_changed:style.editor_block_button_icon}>
+                        <div className={selected==="saturate"?style.editor_block_button_icon_active:Number(params.saturate)!==Number(range.saturate)?style.editor_block_button_icon_changed:style.editor_block_button_icon}>
                             <Image src="/img/saturate.svg" width="36" height="36" alt="icon" />
                         </div>
                         <h6>Saturate</h6>
                     </div>
                     <div onClick={()=>setSelectChoice(prev=>prev='sepia')} className={`${style.editor_block_button} `}>
-                        <div className={selected==="sepia"?style.editor_block_button_icon_active:params.sepia!==range.sepia?style.editor_block_button_icon_changed:style.editor_block_button_icon}>
+                        <div className={selected==="sepia"?style.editor_block_button_icon_active:Number(params.sepia)!==Number(range.sepia)?style.editor_block_button_icon_changed:style.editor_block_button_icon}>
                             <Image src="/img/sepia.svg" width="36" height="36" alt="icon" />
                         </div>
                         <h6>Sepia</h6>
@@ -102,49 +102,49 @@ const ImageEditor = ({lang}) => {
                 <div className={style.editor}>
                     {selected==="blur"&&<div className={`${style.editor_b} block_animation`}>
                         <label htmlFor="cowbell">Blur</label>
-                        <input onChange={(e)=>setRange({...range,blur:e.target.value})} id="cowbell" type="range" defaultValue={range.blur} value={range.blur} min="0" max="16"/>
+                        <input onChange={(e)=>setRange({...range,blur:e.target.value})} id="cowbell" type="range"  value={range.blur}/>
                         <input className={style.enter_number} type="tel" value={range.blur} pattern="(16)|[0-9]\d?" onChange={(e)=>{setRange((v) => (e.target.validity.valid ? {...range,blur:e.target.value} : v))}} />
                     </div>}
                     {selected==="brightness"&&<div className={`${style.editor_b} block_animation`}>
                         <label htmlFor="cowbell">Brightness</label>
-                        <input onChange={(e)=>setRange({...range,brightness:e.target.value})} value={range.brightness} id="cowbell" type="range" defaultValue={range.brightness} min="0" max="200"/>
+                        <input onChange={(e)=>setRange({...range,brightness:e.target.value})} value={range.brightness} id="cowbell" type="range" min="0" max="200"/>
                         <input className={style.enter_number} type="tel" value={range.brightness} pattern="^([0-9]|[1-9][0-9]|[1][0-9][0-9]|20[0-0])$" onChange={(e)=>{setRange((v) => (e.target.validity.valid ? {...range,brightness:e.target.value} : v))}} />
                     </div>}
                     {selected==="contrast"&&<div className={`${style.editor_b} block_animation`}>
                         <label htmlFor="cowbell">Contrast</label>
-                        <input onChange={(e)=>setRange({...range,contrast:e.target.value})} value={range.contrast} id="cowbell" type="range" defaultValue={range.contrast} min="0" max="200"/>
+                        <input onChange={(e)=>setRange({...range,contrast:e.target.value})} value={range.contrast} id="cowbell" type="range" min="0" max="200"/>
                         <input className={style.enter_number} type="tel" value={range.contrast} pattern="^([0-9]|[1-9][0-9]|[1][0-9][0-9]|20[0-0])$" onChange={(e)=>{setRange((v) => (e.target.validity.valid ? {...range,contrast:e.target.value} : v))}} />
                     </div>}
                     {selected==="grayscale"&&<div className={`${style.editor_b} block_animation`}>
                         <label htmlFor="cowbell">Grayscale</label>
-                        <input onChange={(e)=>setRange({...range,grayscale:e.target.value})} value={range.grayscale} id="cowbell" type="range" defaultValue={range.grayscale} min="0" max="100"/>
+                        <input onChange={(e)=>setRange({...range,grayscale:e.target.value})} value={range.grayscale} id="cowbell" type="range" min="0" max="100"/>
                         <input className={style.enter_number} type="tel" value={range.grayscale} pattern="(100)|[0-9]\d?" onChange={(e)=>{setRange((v) => (e.target.validity.valid ? {...range,grayscale:e.target.value} : v))}} />
                     </div>}
                     {selected==="hue"&&<div className={`${style.editor_b} block_animation`}>
                         <label htmlFor="cowbell">Hue-rotate</label>
-                        <input onChange={(e)=>setRange({...range,hue:e.target.value})} value={range.hue} id="cowbell" type="range" defaultValue={range.hue} min="0" max="360"/>
+                        <input onChange={(e)=>setRange({...range,hue:e.target.value})} value={range.hue} id="cowbell" type="range" min="0" max="360"/>
                         <input className={style.enter_number} type="tel" value={range.hue} pattern="^([1-9][0-9]?|[12][0-9][0-9]|3[0-5][0-9]|36[0-5])$" onChange={(e)=>{setRange((v) => (e.target.validity.valid ? {...range,hue:e.target.value} : v))}} />
                     </div>}
                     {selected==="invert"&&<div className={`${style.editor_b} block_animation`}>
                         <label htmlFor="cowbell">Invert</label>
-                        <input onChange={(e)=>setRange({...range,invert:e.target.value})} value={range.invert} id="cowbell" type="range" defaultValue={range.invert} min="0" max="100"/>
+                        <input onChange={(e)=>setRange({...range,invert:e.target.value})} value={range.invert} id="cowbell" type="range" min="0" max="100"/>
                         <input className={style.enter_number} type="tel" value={range.invert} pattern="(100)|[0-9]\d?" onChange={(e)=>{setRange((v) => (e.target.validity.valid ? {...range,invert:e.target.value} : v))}} />
                     </div>}
                     {selected==="saturate"&&<div className={`${style.editor_b} block_animation`}>
                         <label htmlFor="cowbell">Saturate</label>
-                        <input onChange={(e)=>setRange({...range,saturate:e.target.value})} value={range.saturate} id="cowbell" type="range" defaultValue={range.saturate} min="0" max="100"/>
+                        <input onChange={(e)=>setRange({...range,saturate:e.target.value})} value={range.saturate} id="cowbell" type="range" min="0" max="100"/>
                         <input className={style.enter_number} type="tel" value={range.saturate} pattern="(100)|[0-9]\d?" onChange={(e)=>{setRange((v) => (e.target.validity.valid ? {...range,saturate:e.target.value} : v))}} />
                     </div>}
                     {selected==="sepia"&&<div className={`${style.editor_b} block_animation`}>
                         <label htmlFor="cowbell">Sepia</label>
-                        <input onChange={(e)=>setRange({...range,sepia:e.target.value})} value={range.sepia} id="cowbell" type="range" defaultValue={range.sepia} min="0" max="100"/>
+                        <input onChange={(e)=>setRange({...range,sepia:e.target.value})} value={range.sepia} id="cowbell" type="range" min="0" max="100"/>
                         <input className={style.enter_number} type="tel" value={range.sepia} pattern="(100)|[0-9]\d?" onChange={(e)=>{setRange((v) => (e.target.validity.valid ? {...range,sepia:e.target.value} : v))}} />
                     </div>}
                 </div></>}
                 {nav==="rotate"&&<div className={style.editor}>
                 <div className={`${style.editor_b} block_animation`}>
                         <label htmlFor="cowbell">Rotated</label>
-                        <input onChange={(e)=>setRotate(prev=>prev=e.target.value)} id="cowbell" type="range" defaultValue={rotate} value={rotate} min="0" max="360"/>
+                        <input onChange={(e)=>setRotate(prev=>prev=e.target.value)} id="cowbell" type="range" value={rotate} min="0" max="360"/>
                         <input className={style.enter_number} type="tel" value={rotate} pattern="-?[0-9]*" onChange={(e)=>{setRotate((v) => (e.target.validity.valid ? e.target.value : v))}} />
                     </div>
                     </div>}
