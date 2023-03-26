e58fb3796ce6451397bf00719149c5040393dbf6
Dreamer <temirkhan.onyx@gmail.com>
2023-03-26 16:25:25 +0600

    asddasadsasdadsads

--- pages/technology/image-editor/index.jsx
+++ pages/technology/image-editor/index.jsx
@@ -2,7 +2,7 @@ import dynamic from "next/dynamic";
 import NavPreloader from "/modules/navbar_app/nav_preloader";
 const NavbarApp = dynamic(()=>import('/modules/navbar_app/nav'),{ssr:false,loading:NavPreloader});
 import style from "/styles/technology/image-editor/index.module.css";
-import { useState,useRef,useEffect } from "react";
+import { useState,useRef,useEffect,useMemo } from "react";
 import Image from "next/image";
 export const getStaticProps = async ({locale}) => {
     return {props:{lang:locale}};
@@ -30,74 +30,75 @@ const ImageEditor = ({lang}) => {
     useEffect(()=>{
         if(imageEditor!==undefined) imageEditor.current.style.cssText = `filter:blur(${range.blur}px) brightness(${range.brightness}%) contrast(${range.contrast}%) grayscale(${range.grayscale}%) hue-rotate(${range.hue}deg) invert(${range.invert}%) saturate(${range.saturate+1}) sepia(${range.sepia}%);transform:rotate(${range.rotate}deg);`;
     },[range]);
-    return(
-        <>
-        <NavbarApp lang={lang} choice="alone"/>
-        <div className="main_app">
-            <div className="main_block_row">
-                <div className={style.image_block}>
-                    <Image ref={imageEditor} className={style.check} src="/img/editor.jpg" width={600} height={300} alt="editor" />
-                </div>
-                <div className={style.editor_nav}>
-                    <div onClick={()=>setSelectChoice({...selected,nav:"correction"})} className={selected.nav==="correction"?style.editor_nav_block_active:style.editor_nav_block}>
-                        Коррекция
-                    </div>
-                    <div onClick={()=>setSelectChoice({...selected,nav:"filter"})} className={selected.nav==="filter"?style.editor_nav_block_active:style.editor_nav_block}>
-                        Фильтры
-                    </div>
-                    <div onClick={()=>setSelectChoice({...selected,nav:"rotate"})} className={selected.nav==="rotate"?style.editor_nav_block_active:style.editor_nav_block}>
-                        Выпрямление
-                    </div>
-                </div>
-                {selected.nav==="correction"&&<><div className={`${style.editor_block} block_animation`}>
-                    <div onClick={()=>setSelectChoice({...selected,correction:'blur'})} className={`${style.editor_block_button} `}>
-                        <div className={selected.correction==="blur"?style.editor_block_button_icon_active:Number(params.blur)===Number(range.blur)?style.editor_block_button_icon:style.editor_block_button_icon_changed}>
-                            <Image src="/img/blur.svg" width="36" height="36" alt="icon" />
+    const navMenu = useMemo(()=>{
+        const navText = [{text:"Коррекция",nav:"correction"},{text:"Фильтры",nav:"filter"},{text:"Выпрямление",nav:"rotate"}];
+        return(
+            <div className={style.editor_nav}>
+                {navText.map((result)=><div onClick={()=>setSelectChoice({...selected,nav:result.nav})} className={selected.nav===result.nav?style.editor_nav_block_active:style.editor_nav_block}>
+                    {result.text}
+                </div>)}
+            </div>
+        );
+    },[selected]);
+    const correctionMenu = useMemo(()=>{
+        const correctionText = [
+            {
+                text:"Blur",
+                image:"/img/blur.svg",
+                key:"blur" 
+            },
+            {
+                text:"Brightness",
+                image:"/img/brightness.svg",
+                key:"brightness"
+            },
+            {
+                text:"Contrast",
+                image:"/img/contrast.svg",
+                key:"contrast",
+            },
+            {
+                text:"Grayscale",
+                image:"/img/contrast.svg",
+                key:"grayscale"
+            },
+            {
+                text:"Hue-rotate",
+                image:"/img/hue.svg",
+                key:"hue"
+            },
+            {
+                text:"Invert",
+                image:"/img/hue.svg",
+                key:"invert"
+            },
+            {
+                text:"Saturate",
+                image:"/img/saturate.svg",
+                key:"saturate"
+            },
+            {
+                text:"Saturate",
+                image:"/img/saturate.svg",
+                key:"saturate"
+            },
+            {
+                text:"Sepia",
+                image:"/img/sepia.svg",
+                key:"sepia"
+            }
+        ]
+        return(
+            selected.nav==="correction"&&<><div className={`${style.editor_block} block_animation`}>
+                {correctionText.map((result)=>
+                    <div onClick={()=>setSelectChoice({...selected,correction:result.key})} className={`${style.editor_block_button} `}>
+                        <div className={selected.correction===result.key?style.editor_block_button_icon_active:Number(params[result.key])===Number(range[result.key])?style.editor_block_button_icon:style.editor_block_button_icon_changed}>
+                            <Image src={result.image} width="36" height="36" alt="icon" />
                         </div>
-                        <h6>Blur</h6>
+                        <h6>{result.text}</h6>
                     </div>
-                    <div onClick={()=>setSelectChoice({...selected,correction:'brightness'})} className={`${style.editor_block_button} `}>
-                        <div className={selected.correction==="brightness"?style.editor_block_button_icon_active:Number(params.brightness)!==Number(range.brightness)?style.editor_block_button_icon_changed:style.editor_block_button_icon}>
-                            <Image src="/img/brightness.svg" width="36" height="36" alt="icon" />
-                        </div>
-                        <h6>Brightness</h6>
-                    </div>
-                    <div onClick={()=>setSelectChoice({...selected,correction:'contrast'})} className={`${style.editor_block_button} `}>
-                        <div className={selected.correction==="contrast"?style.editor_block_button_icon_active:Number(params.contrast)!==Number(range.contrast)?style.editor_block_button_icon_changed:style.editor_block_button_icon}>
-                            <Image src="/img/contrast.svg" width="36" height="36" alt="icon" />
-                        </div>
-                        <h6>Contrast</h6>
-                    </div>
-                    <div onClick={()=>setSelectChoice({...selected,correction:'grayscale'})} className={`${style.editor_block_button} `}>
-                        <div className={selected.correction==="grayscale"?style.editor_block_button_icon_active:Number(params.grayscale)!==Number(range.grayscale)?style.editor_block_button_icon_changed:style.editor_block_button_icon}>
-                            <Image src="/img/contrast.svg" width="36" height="36" alt="icon" />
-                        </div>
-                        <h6>Grayscale</h6>
-                    </div>
-                    <div onClick={()=>setSelectChoice({...selected,correction:'hue'})} className={`${style.editor_block_button} `}>
-                        <div className={selected.correction==="hue"?style.editor_block_button_icon_active:Number(params.hue)!==Number(range.hue)?style.editor_block_button_icon_changed:style.editor_block_button_icon}>
-                            <Image src="/img/hue.svg" width="36" height="36" alt="icon" />
-                        </div>
-                        <h6>Hue-rotate</h6>
-                    </div>
-                    <div onClick={()=>setSelectChoice({...selected,correction:'invert'})} className={`${style.editor_block_button} `}>
-                        <div className={selected.correction==="invert"?style.editor_block_button_icon_active:Number(params.invert)!==Number(range.invert)?style.editor_block_button_icon_changed:style.editor_block_button_icon}>
-                            <Image src="/img/hue.svg" width="36" height="36" alt="icon" />
-                        </div>
-                        <h6>Invert</h6>
-                    </div>
-                    <div onClick={()=>setSelectChoice({...selected,correction:'saturate'})} className={`${style.editor_block_button} `}>
-                        <div className={selected.correction==="saturate"?style.editor_block_button_icon_active:Number(params.saturate)!==Number(range.saturate)?style.editor_block_button_icon_changed:style.editor_block_button_icon}>
-                            <Image src="/img/saturate.svg" width="36" height="36" alt="icon" />
-                        </div>
-                        <h6>Saturate</h6>
-                    </div>
-                    <div onClick={()=>setSelectChoice({...selected,correction:'sepia'})} className={`${style.editor_block_button} `}>
-                        <div className={selected.correction==="sepia"?style.editor_block_button_icon_active:Number(params.sepia)!==Number(range.sepia)?style.editor_block_button_icon_changed:style.editor_block_button_icon}>
-                            <Image src="/img/sepia.svg" width="36" height="36" alt="icon" />
-                        </div>
-                        <h6>Sepia</h6>
-                    </div>
+                )
+                    }
                 </div>
                 <div className={style.editor}>
                     {selected.correction==="blur"&&<div className={`${style.editor_b} block_animation`}>
@@ -140,7 +141,19 @@ const ImageEditor = ({lang}) => {
                         <input onChange={(e)=>setRange({...range,sepia:e.target.value})} value={range.sepia} id="cowbell" type="range" min="0" max="100"/>
                         <input className={style.enter_number} type="tel" value={range.sepia} pattern="(100)|[0-9]\d?" onChange={(e)=>{setRange((v) => (e.target.validity.valid ? {...range,sepia:e.target.value} : v))}} />
                     </div>}
-                </div></>}
+                </div></>
+        );
+    },[selected,range]);
+    return(
+        <>
+        <NavbarApp lang={lang} choice="alone"/>
+        <div className="main_app">
+            <div className="main_block_row">
+                <div className={style.image_block}>
+                    <Image ref={imageEditor} className={style.check} src="/img/editor.jpg" width={600} height={300} alt="editor" />
+                </div>
+                {navMenu}
+                {correctionMenu}
                 {selected.nav==="rotate"&&<div className={style.editor}>
                 <div className={`${style.editor_b} block_animation`}>
                         <label htmlFor="cowbell">Rotated</label>
