23671ded9080976a70d978ea6b5f928e7dc95828
Dreamer <temirkhan.onyx@gmail.com>
2023-03-26 21:56:59 +0600

    sfdfdssdfdsf

--- pages/technology/image-editor/index.jsx
+++ pages/technology/image-editor/index.jsx
@@ -165,8 +165,10 @@ const ImageEditor = ({lang}) => {
         <NavbarApp lang={lang} choice="alone"/>
         <div className="main_app">
             <div className="main_block_row">
+                <div className={style.image_editor}>
                 <div className={style.image_block_port}>
                     <Image ref={imageEditor} className={style.check_port} src="/img/editor_port.jpg" width={600} height={300} alt="editor" />
+                </div>
                 </div>
                 {navMenu}
                 {correctionMenu}
