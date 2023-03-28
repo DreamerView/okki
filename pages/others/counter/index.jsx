ae0ebad5a8509cbcfa10de450ff594946c1af408
Dreamer <temirkhan.onyx@gmail.com>
2023-03-28 15:49:29 +0600

    sfdsfdsfsd

--- pages/others/counter/index.jsx
+++ pages/others/counter/index.jsx
@@ -10,11 +10,11 @@ export const getStaticProps = async ({locale}) => {
 
 const CounterApp = ({lang}) => {
     const [counter,setCounter] = useState(0);
-    const addCount = () => {
+    const addCount = async() => {
         new Audio("/audio/click-button.mp3").play();
         return setCounter(prev=>prev+1);
     };
-    const resetCount = () => {
+    const resetCount = async() => {
         new Audio("/audio/click-button.mp3").play();
         return setCounter(prev=>prev=0);
     };
