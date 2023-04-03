56bfc4f2bb646a37596d47b168abadb5f9d7dac3
Dreamer <temirkhan.onyx@gmail.com>
2023-04-03 06:23:01 +0600

    fsdfdssfdfsd

--- pages/others/clock/alarm/index.jsx
+++ pages/others/clock/alarm/index.jsx
@@ -5,12 +5,19 @@ import AppPreloader from "@/modules/app_store/app_preloader";
 const AppShow =  dynamic(()=>import("@/modules/app_store/app"),{loading: AppPreloader});
 import style from "@/styles/technology/clock/index.module.css";
 import Link from "next/link";
+import { useState,useRef } from "react";
 
 export const getStaticProps = async ({locale}) => {
     return {props:{lang:locale}};
 };
 
 const AlarmApp = ({lang}) => {
+    const inputAlarm = useRef();
+    const [alarm,setAlarm] = useState([]);
+    const addAlarm = (e) => {
+        e.preventDefault();
+        setAlarm([...alarm,{time:inputAlarm.current.value}]);
+    };
     return(<>
     <NavbarApp lang={lang} choice="alone"/>
         <div className="main_app ">
@@ -34,22 +41,18 @@ const AlarmApp = ({lang}) => {
                 </div>
                 <div className={style.clock}>
                     <div className={style.clock_panel}>
-                        <input type="time" className={`${style.alarm} time`} name="appt" defaultValue="00:00" />
+                        <input ref={inputAlarm} type="time" className={`${style.alarm} time`} name="appt" defaultValue="00:00" />
                     </div>
                     <div className={style.clock_panel}>
                         Alarm
                     </div>
                 </div>
                 <div className={style.alarm_block}>
-                    <button className={style.add_alarm}>Add alarm</button>
+                    <button type="button" onClick={addAlarm} className={style.add_alarm}>Add alarm</button>
                     <button className={style.more}>Settings</button>
                 </div>
-                <div className={style.stopwatch_action}>
-                    {/* <button type="button" className={action===false?style.stopwatch_action_stop:style.stopwatch_action_reset} onClick={()=>action===true?writeResult():resetSec()}>{action===true?"Round":"Reset"}</button>
-                    <button type="button" className={action===false?style.stopwatch_action_start:style.stopwatch_action_stop} onClick={()=>setAction(action===false?true:false)}>{action===false?"Start":"Stop"}</button> */}
-                </div>
                 <div className={style.clock_info}>
-                    {/* {result!==0&&result.map((res,index)=><div key={index}><span>Круг {index+1}</span><p>{res}</p></div>)} */}
+                    {alarm.length!==0&&alarm.map((res,index)=><div key={index}><span>Время</span><p>{res.time}</p></div>)}
                 </div>
             </div>
         </div>
