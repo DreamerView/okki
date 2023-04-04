ddc0ecd61d48b7518de8d3a9b042c0932c442aa0
DreamerView <temirkhan.onyx@gmail.com>
2023-04-04 16:06:51 +0600

    sfddsfsdfsdf

--- start/conf.js
+++ start/conf.js
@@ -3,8 +3,7 @@ import { useEffect } from "react";
 import { useSelector,useDispatch } from "react-redux";
 
 const useConfirm = (res) => {
-    const dis = useDispatch();
-    const result = useSelector(state=>state.confirm);
+    const dis = useDispatch(),result = useSelector(state=>state.confirm);
 
     useEffect(() => {
         if(res) dis({type:"SetAction",set:{name:res.name,content:res.content,type:res.type,text:{cancel:res.cancel,accept_color:res.accept_color,accept:res.accept}}});
