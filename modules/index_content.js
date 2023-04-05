793d3cb9d3c434dd5eb955afb35904483db6081a
DreamerView <temirkhan.onyx@gmail.com>
2023-04-06 00:07:19 +0600

    fsddsfsdfsfd

--- modules/index_content.js
+++ modules/index_content.js
@@ -3,8 +3,8 @@ import dynamic from "next/dynamic";
 import AppStorePreloader from "@/modules/app_store/apps_preloader";
 const AppStore =  dynamic(()=>import("@/modules/app_store/apps"),{loading: AppStorePreloader});
 const AppList =  dynamic(()=>import("@/modules/app_store/app_list"),{loading: AppStorePreloader});
-// const SubAppStore =  dynamic(()=>import("@/modules/app_store/subApps"),{loading: AppStorePreloader});
-// const SubAppList =  dynamic(()=>import("@/modules/app_store/subAppList"),{loading: AppStorePreloader});
+const SubAppStore =  dynamic(()=>import("@/modules/app_store/subApps"),{loading: AppStorePreloader});
+const SubAppList =  dynamic(()=>import("@/modules/app_store/subAppList"),{loading: AppStorePreloader});
 import Image from "next/image";
 import translate from "@/translate/index_translate";
 import all from '/start/services/subCategory.json';
@@ -19,8 +19,10 @@ const IndexContent = ({lang}) => {
             {/* <hr/> */}
             <h1>{translate!==undefined&&translate['try_use'][lang]}</h1>
               <AppStore lang={lang}/>
-              {/* {all.map((e,index)=>
-              index%2===0?<SubAppList key={index} lang={lang} category={e.name} />:<SubAppStore key={index} lang={lang} category={e.name} />)} */}
+              {all.map((e,index)=>
+              <div key={index}>
+              {index%2===0?<SubAppList key={index} lang={lang} category={e.name} />:<SubAppStore key={index} lang={lang} category={e.name} />}
+              </div>)}
           </div>
       </div>
   )
