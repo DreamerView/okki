fd7dffb87a955d31a468df97407a87ba54505db2
DreamerView <temirkhan.onyx@gmail.com>
2023-04-06 00:15:39 +0600

    sfdfdsfdssdf

--- modules/index_content.js
+++ modules/index_content.js
@@ -3,8 +3,9 @@ import dynamic from "next/dynamic";
 import AppStorePreloader from "@/modules/app_store/apps_preloader";
 const AppStore =  dynamic(()=>import("@/modules/app_store/apps"),{loading: AppStorePreloader});
 const AppList =  dynamic(()=>import("@/modules/app_store/app_list"),{loading: AppStorePreloader});
-const SubAppStore =  dynamic(()=>import("@/modules/app_store/subApps"),{loading: AppStorePreloader});
-const SubAppList =  dynamic(()=>import("@/modules/app_store/subAppList"),{loading: AppStorePreloader});
+// const SubAppStore =  dynamic(()=>import("@/modules/app_store/subApps"),{loading: AppStorePreloader});
+import SubAppStore from "@/modules/app_store/subApps";
+// const SubAppList =  dynamic(()=>import("@/modules/app_store/subAppList"),{loading: AppStorePreloader});
 import Image from "next/image";
 import translate from "@/translate/index_translate";
 import all from '/start/services/subCategory.json';
@@ -21,7 +22,7 @@ const IndexContent = ({lang}) => {
               <AppStore lang={lang}/>
               {all.map((e,index)=>
               <div key={index}>
-              <SubAppStore key={index} lang={lang} category={e.name} />
+              <SubAppStore lang={lang} category={e.name} />
               </div>)}
           </div>
       </div>
